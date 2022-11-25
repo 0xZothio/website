a1c271bdb47b6c976aaf1811384cf6f195e5cc40
srinivastejavt26 <thirusrinivasa@quickwork.co>
2022-11-25 18:36:50 +0530

    Update Slider.js

--- src/Utils/Slider.js
+++ src/Utils/Slider.js
@@ -1,6 +1,11 @@
 import React from "react";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
+import lapi1 from "../assets/1.jpeg";
+import lapi2 from "../assets/2.jpeg";
+import lapi3 from "../assets/3.jpeg";
+import lapi4 from "../assets/4.jpeg";
+import lapi5 from "../assets/5.jpeg";
 const Slider = () => {
   return (
     <>
@@ -15,19 +20,19 @@ const Slider = () => {
         height="80%"
       >
         <div className=" rounded-3">
-          <img className="rounded-3" style={{width:"650px", height:"400px",boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)" }} src="https://resources.zoth.in/cre/images/101/1.webp" />
+          <img className="rounded-3" style={{width:"650px",boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)" }} src={lapi1} />
         </div>
         <div className="  rounded-3">
-          <img className="rounded-3" style={{width:"650px", height:"400px", boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)"}} src="https://resources.zoth.in/cre/images/102/1.webp" />
+          <img className="rounded-3" style={{width:"650px", boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)"}} src={lapi2} />
         </div>
         <div className="  rounded-3">
-          <img className="rounded-3" style={{width:"650px", height:"400px", boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)"}} src="https://resources.zoth.in/cre/images/104/1.webp" />
+          <img className="rounded-3" style={{width:"650px", boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)"}} src={lapi3} />
         </div>
         <div className="  rounded-3">
-          <img className="rounded-3" style={{width:"650px", height:"400px", boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)"}} src="https://resources.zoth.in/cre/images/105/1.webp" />
+          <img className="rounded-3" style={{width:"650px", boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)"}} src={lapi4} />
         </div>
         <div className="  rounded-3">
-          <img className="rounded-3" style={{width:"650px", height:"400px", boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)"}} src="https://resources.zoth.in/cre/images/106/1.webp" />
+          <img className="rounded-3" style={{width:"650px", boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)"}} src={lapi5} />
         </div>
       </Carousel>
     </>
