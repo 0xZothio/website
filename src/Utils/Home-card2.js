9f1f12a3e1e532ddf458123e048e5dd575d064f3
srinivastejavt26 <thirusrinivasa@quickwork.co>
2022-11-25 23:41:26 +0530

    Create Home-card2.js

---
+++ src/Utils/Home-card2.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Homecard2 = ({ img,title,summary }) => {
+  return (
+    <>
+      <div className="col-md-5 col-8 home-card rounded-3 p-4  col-xl-5 col-xs-5 mx-auto mt-5">
+        <div className="d-flex justify-content-center align-items-center overflow-hidden bg-primary">
+          {img}
+        </div>
+        <h4
+          className="heading text-center text-dark mt-4"
+          style={{ fontWeight: "700", fontSize: "15px" }}
+        >
+          {title}
+        </h4>
+        <p
+          className="text-dark text-center mt-2"
+          style={{ fontWeight: "500", fontSize: "11px" }}
+        >
+          {summary}
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default Homecard2;
