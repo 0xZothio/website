6dad6087a588e891a8edbf6670c6b0d7eff59379
srinivastejavt26 <thirusrinivasa@quickwork.co>
2022-11-25 21:48:54 +0530

    Update Property.js

--- src/Screens/Property.js
+++ src/Screens/Property.js
@@ -10,6 +10,8 @@ import lapi2 from "../assets/2.jpeg";
 import lapi3 from "../assets/3.jpeg";
 import lapi4 from "../assets/4.jpeg";
 import lapi5 from "../assets/5.jpeg";
+
+import danta from "../assets/dantalogo.png";
 const Property = () => {
   let { openModal, closeModal, customStyles, modalIsOpen } =
     useContext(DataContext);
@@ -23,21 +25,42 @@ const Property = () => {
       >
         <Subscribe />
       </Modal>
-      <div className="row">
-        <div className="col-md-10 col-12 col-xl-10 col-lg-10 col-xs-12 col-sm-12 mx-auto d-flex justify-content-center align-items-center flex-column ">
-          <h2 className="home-heading mt-5 text-center">
-            How Our MarketPlace Works
-          </h2>
-          <p className="text-center text-capitalize">
-            {" "}
-            <span className="text-primary">____</span>
-            Premeium Assets
-            <span className="text-primary">____</span>
-          </p>
-          <h1 className="text-center mt-5 fw-bold" style={{ color: "#007AFF" }}>
-            927 Euclid Ave Apt 10 - Miami Beach, FL 33139)
+      <div className="row mt-2">
+      <div className="col-md-8 col-12 col-xl-8 col-lg-8 col-sm-12 col-xs-12 mx-auto p-5 w-100 h-auto" style={{
+          background:
+          "linear-gradient(90deg, #8E2DE2 0%, #4A00E0 100%)",
+          boxShadow:
+            "0 4px 8px 0 rgba(0, 0, 0, 0.4), 0 6px 20px 0 rgba(0, 0, 0, 0.4)",
+          height:"500px"
+        }}>
+          
+
+          <div className="row">
+          <img src={danta} className="mx-auto d-block" alt="" style={{width:"200px", height:"200px"}}/>
+          </div>
+          <div className="row mt-4">
+            <p className="text-center text-white" style={{fontSize:"18px",fontWeight:"600"}}>Danta group is promoted by B. Raghavendra, having more than 30 years of
+experience in banking, real estate and hospitality industry. The group has
+successfully completed an expansive coffee estate project ‘Danta Estate’ near
+Sakaleshpur and the project is handed over to Owners' association for
+maintenance.<br></br> <br></br>
+Group is operating a home stay in Danta Estate built in malenad architecture in
+the middle of soul stirring ambiance blessed with natural streams. A privileged
+stay in the estate house has attracted many nature lovers in the last 7 years.
+
+</p>
+          </div>
+
+          
+        </div>
+        <div className="col-md-10 col-12 col-xl-10 col-lg-10 col-xs-12 col-sm-12 mx-auto d-flex justify-content-center align-items-center flex-column " >
+          
+          <h1 className="headi-prop text-center mt-5 fw-bold" style={{fontSize:"28px",color:"#053786", width:"75%"}}>
+          Danta Resort, Madikeri, Coorg, Karnataka
           </h1>
-          <hr style={{ height: "2.5px !important" }} className="text-primary" />
+          <p className="headi-prop text-center mt-2" style={{fontSize:"22px", color:"#053786", fontWeight:"400"}}>Rejuvenate Yourself With Nature</p>
+          
+          <hr style={{ height: "2.5px !important" }} className="text-primary mt-3 mx-auto d-block" />
           <Carousel
             autoPlay={true}
             infiniteLoop={true}
@@ -47,11 +70,11 @@ const Property = () => {
             showStatus={false}
             className="mt-2 h-75"
           >
-            <div className=" rounded-3">
+            <div className=" rounded-3 ">
               <img
                 className="rounded-3"
                 style={{
-                  width: "750px",
+                  width: "800px",
                   height: "200px",
                 }}
                 src={lapi1}
@@ -61,7 +84,7 @@ const Property = () => {
               <img
                 className="rounded-3"
                 style={{
-                  width: "750px",
+                  width: "800px",
                   height: "200px",
                 }}
                 src={lapi2}
@@ -71,7 +94,7 @@ const Property = () => {
               <img
                 className="rounded-3"
                 style={{
-                  width: "750px",
+                  width: "800px",
                   height: "200px",
                 }}
                 src={lapi3}
@@ -81,7 +104,7 @@ const Property = () => {
               <img
                 className="rounded-3"
                 style={{
-                  width: "750px",
+                  width: "800px",
                   height: "200px",
                 }}
                 src={lapi4}
@@ -91,19 +114,16 @@ const Property = () => {
               <img
                 className="rounded-3"
                 style={{
-                  width: "750px",
+                  width: "800px",
                   height: "200px",
                 }}
                 src={lapi5}
               />
             </div>
           </Carousel>
-          <p className="text-center mt-4">
-            This property is a stunning two-story Townhome in a boutique
-            building in the heart of South Beach.
-          </p>
+          
         </div>
-        <div className="row mt-4 justify-content-center">
+        {/* <div className="row mt-4 justify-content-center">
           <div className="col-md-4 col-xl-4 col-lg-4 col-12 col-sm-12 col-xs-12 mx-auto d-flex justify-content-between">
             <div className="d-flex flex-column">
               <RiWechatLine size={30} color="black" />
@@ -135,62 +155,43 @@ const Property = () => {
               </button>
             </div>
           </div>
-        </div>
-        <div className="row mt-5 mb-3">
+        </div> */}
+        <div className="col-md-8 col-12 col-xl-8 col-lg-8 col-sm-12 col-xs-12 mx-auto p-5 w-100 h-auto mt-5 mb-3" style={{
+          background:
+          "linear-gradient(90deg, #8E2DE2 0%, #4A00E0 100%)",
+          boxShadow:
+            "0 4px 8px 0 rgba(0, 0, 0, 0.4), 0 6px 20px 0 rgba(0, 0, 0, 0.4)",
+          height:"500px"
+        }}>
+          
+
+          
+          <div className="row">
           <h1 className="property-heading justify-content-center text-center mt-2">
             {" "}
-            <span className="text-primary">____</span>
-            About Property
-            <span className="text-primary">____</span>
+            <p className="text-center text-white">About Property</p>
           </h1>
+            <p className="text-center text-white" style={{fontSize:"18px",fontWeight:"600"}}>Danta Resort is the second project of the group, set in the middle of hills and
+rivers in Coorg, also known as Land of warriors. Located 24 km from Madikeri
+town in western ghats is sure to persuade you to be an aspiring holiday
+destination. Amenities in the resort include spacious rooms with large sit outs
+facing hills and river, multi-cuisine restaurant, indoor games, outdoor
+activities and jogging track along the river.<br></br> <br></br>
+Inspired by nature, Danta group has launched a unique residential
+layout with 68 cottages in the middle of spices plantation
+surrounded by a river. These cottages are designed in Malenad
+Architecture to open up your senses to a rich and mystical
+experience of nature.
+Danta vacation home is an exceptional investment destination,
+offers a genuine opportunity to own a cottage with a combination of
+excellent capital appreciation, life time free accommodation and a
+source for revenue generation.
 
-          <div className="col-md-10 col-12 col-xl-10 col-lg-10 col-xs-12 col-sm-12 mx-auto mt-5 text-center d-flex justify-content-center mb-5">
-            <div className="d-flex flex-column mt-3 text-center"> 
-              <h1 className="text-center fw-bold">
-                <strong>INTRODUCTION</strong>
-              </h1>
-              <p className="text-center mx-sm-3 mx-xs-3 mt-2">
-                This Miami Beach condo was recently featured on HGTV’s "My
-                Lottery Dream Home" Season 13 Episode 10: South Beach Birth
-                Rite, which first aired September 23, 2022.<br></br> <br></br>
-                This property was unaffected by Hurricane Ian and comes with
-                flood insurance and hurricane impact windows.<br></br> <br></br>
-                Stunning two-story Townhome in a boutique building in the heart
-                of South Beach. This apartment building was converted into 10
-                two-story townhomes back in 2012. This 2-bedroom, 1.5-bathroom
-                home is a corner unit, including modern eat-in kitchen,
-                stainless steel appliances, washer/dryer, one A/C unit for each
-                floor, and hurricane impact windows. Property is steps away from
-                Ocean Drive, sandy beaches, and the Atlantic Ocean. The Art Deco
-                district with Espanola Way & Lincoln Road are within walking
-                distance. Unit includes appliances, blinds, and furniture.
-                <br></br> <br></br>
-                Tenant is on a 1 year lease from 07/26/2022 - 07/31/2023 and is
-                paying $3,250/month with a $6,500 deposit.
-              </p>
-            </div>
-
-            {/* <div className="d-flex flex-column mt-5">
-                <h1 className="text-center">
-                    <strong>PRICE TREND</strong>
-                </h1>
-                <p className="text-center">
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequuntur consectetur dolor expedita optio. Magnam deleniti corporis libero vitae, nulla ducimus voluptatum laudantium aut, et, officiis sunt nobis doloribus vero nam?
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos commodi alias exercitationem aliquid dolores facere ex delectus officia cum laborum nemo odit aut voluptas dicta tenetur hic velit, impedit nulla.
-                </p>
-            </div>
-
-            <div className="d-flex flex-column mt-5">
-                <h1 className="text-center">
-                    <strong>LOCATION</strong>
-                </h1>
-                <p className="text-center">
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequuntur consectetur dolor expedita optio. Magnam deleniti corporis libero vitae, nulla ducimus voluptatum laudantium aut, et, officiis sunt nobis doloribus vero nam?
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos commodi alias exercitationem aliquid dolores facere ex delectus officia cum laborum nemo odit aut voluptas dicta tenetur hic velit, impedit nulla.
-                </p>
-            </div> */}
+</p>
           </div>
         </div>
+        
+        
       </div>
     </>
   );
