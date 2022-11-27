759027596d819d742b3bdad2bf8d373a4b406438
srinivastejavt26 <thirusrinivasa@quickwork.co>
2022-11-27 12:20:19 +0530

    Update Slider.js

--- src/Utils/Slider.js
+++ src/Utils/Slider.js
@@ -1,6 +1,11 @@
 import React from "react";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
+import pic1 from "../assets/1.jpeg";
+import pic2 from "../assets/2.jpeg";
+import pic3 from "../assets/3.jpeg";
+import pic4 from "../assets/4.jpeg";
+import pic5 from "../assets/5.jpeg";
 const Slider = () => {
   return (
     <>
@@ -15,19 +20,19 @@ const Slider = () => {
         height="80%"
       >
         <div className=" rounded-3">
-          <img className="rounded-3" style={{width:"650px",boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)" }} src="https://resources.zoth.in/website/1.jpeg" />
+          <img className="rounded-3" style={{width:"650px",boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)" }} src={pic1} />
         </div>
         <div className="  rounded-3">
-          <img className="rounded-3" style={{width:"650px", boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)"}} src="https://resources.zoth.in/website/2.jpeg" />
+          <img className="rounded-3" style={{width:"650px", boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)"}} src={pic2} />
         </div>
         <div className="  rounded-3">
-          <img className="rounded-3" style={{width:"650px", boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)"}} src="https://resources.zoth.in/website/3.jpeg" />
+          <img className="rounded-3" style={{width:"650px", boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)"}} src={pic3} />
         </div>
         <div className="  rounded-3">
-          <img className="rounded-3" style={{width:"650px", boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)"}} src="https://resources.zoth.in/website/4.jpeg" />
+          <img className="rounded-3" style={{width:"650px", boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)"}} src={pic4} />
         </div>
         <div className="  rounded-3">
-          <img className="rounded-3" style={{width:"650px", boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)"}} src="https://resources.zoth.in/website/5.jpeg" />
+          <img className="rounded-3" style={{width:"650px", boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)"}} src={pic5} />
         </div>
       </Carousel>
     </>
